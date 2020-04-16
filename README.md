ac63790a4eea0c1d93f8810e54e46ae247bf3108
4856742@qq.com <4856742@qq.com>
2020-04-16 22:05:50 +0800

    更新框架

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
+# [龙啸轩内容管理系统 V3.0](http://www.cxuu.net)
+
+此版本是基于 Z-PHP 和 layui构建，简单高效！！简单粗暴！！
+
+php 7.1+ 
+
+mysql 5.6+
+
+开发文档请看：
+
 # [Z-PHP框架](http://www.z-php.com)
 [文档](https://www.showdoc.cc/zphp4):https://www.showdoc.cc/zphp4
 
-基于MVC模式的轻量级PHP框架，框架核心只有十几个文件，100多KB的大小。
+使用文档？
+在程序内部和模板上都有相应注释，我比较懒，暂时没有整理出来。
+
+后台访问地址：xxx.com/admin.php
+后台用户名：admin  密码：123456
+
+数据库是根目录的cxuuweb.sql，直接导入数据库即可，数据库配置文件在根目录的common/config.php里！
+
+WEB目录请绑定在：/public
+
+特别注意：
+
+1、前端默认开启了redis缓存及路由模式，需要运行环境支持！
+2、网站要要求环境伪静态支持！
--- app/admin/v1.0/model/attments.class.php
+++ app/admin/v1.0/model/attments.class.php
@@ -5,15 +5,6 @@ use root\base\model;
 
 class attments extends model
 {
-	//获取数据
-    public function FindData()
-    {
-        $db = $this->db();
-		$where['name'] = 'siteinfo';
-		$result = $db->table('attments')->where($where)->find();
-		$config = unserialize($result['data']);
-        return $config;
-    }
 	
 	//添加数据
 	public function insertData($atturl,$priname,$ext,$size)
@@ -32,31 +23,5 @@ class attments extends model
 			$result = 'error';
 		}
         return $result;
-    }
-	
-	//插入数据
-	public function updateData()
-    {
-        $db = $this->db();
-        $data = [
-			'sitename' => $_POST['sitename'],
-			'siteurl' => $_POST['siteurl'],
-			'keywords' => $_POST['keywords'],
-			'descript' => $_POST['descript'],
-			'copyright' => $_POST['copyright'],
-			'uploadsize' => $_POST['uploadsize'],
-			'uploadext' => $_POST['uploadext'],
-			];
-		$where['name'] = 'siteinfo';
-		$ser_data['data'] = serialize($data);
-
-        $re_key = $db->table('attments')->Where($where)->Update($ser_data);
-		if($re_key){
-			$result = ['status' => 1,'msg' => '修改成功'];
-		}else{
-			$result = ['status' => 0,'msg' => '数据没有变化！'];
-		}
-        return $result;
-    }
-	
+    }	
 }
