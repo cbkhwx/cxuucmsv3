8a5c82d00ea795905954e8486cd1e6184640492a
4856742@qq.com <4856742@qq.com>
2020-04-19 12:33:55 +0800

    小调整

--- app/index/v1.0/ctrl/contents.class.php
+++ app/index/v1.0/ctrl/contents.class.php
@@ -10,29 +10,37 @@ class contents extends ctrl
 {
     public static function index()
     {
+
 		if(!isset($_GET['cid']) & empty($_GET['cid']) & empty(ROUTE['params']['cid'])){
 			return  parent::_404();
 		}
+
+		$html = view::GetCache(600000, 'index.html',true); 
+
+		if($html){
+			echo $html;
+		} else {
 		
-		$d = new article;
-		$list = $d->listData();
-		
-		if(empty($list['data'])){
-			return  parent::_404();
-		}
-		
-		$d = new common;
-		$findCate = $d->findCateData($list['cid']);
-		
-		view::assign('list',$list['data']);
-		view::assign('page',$list['page']);
-		view::assign('cid',$list['cid']);
-		view::assign('catename',$findCate['name']);
-		if(empty($findCate['theme'])){
-			view::display('',60,true);
-		}else{
-			view::display($findCate['theme'],60,true);
-		}
+			$d = new article;
+			$list = $d->listData();
+			
+			if(empty($list['data'])){
+				return  parent::_404();
+			}
+			
+			$d = new common;
+			$findCate = $d->findCateData($list['cid']);
+			
+			view::assign('list',$list['data']);
+			view::assign('page',$list['page']);
+			view::assign('cid',$list['cid']);
+			view::assign('catename',$findCate['name']);
+			if(empty($findCate['theme'])){
+				view::display();
+			}else{
+				view::display($findCate['theme']);
+			}
+		}	
 		
     }
 }
