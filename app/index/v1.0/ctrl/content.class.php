0263b0fc260dd6081111449d7ceb7b1721c7ad54
4856742@qq.com <4856742@qq.com>
2020-04-19 11:18:09 +0800

    更新框架

--- app/index/v1.0/ctrl/content.class.php
+++ app/index/v1.0/ctrl/content.class.php
@@ -6,29 +6,29 @@ use model\article;
 use model\common;
 use z\view;
 
-class content extends ctrl
-{
-    public static function index()
-    {
- 		if(!isset($_GET['id']) & empty($_GET['id']) & empty(ROUTE['params']['id'])){
-			return  parent::_404();
-		}
+class content extends ctrl {
+    public static function index() {
+        if ( !isset( $_GET['id'] ) & empty( $_GET['id'] ) & empty( ROUTE['params']['id'] ) ) {
+            return  parent::_404();
+        }
 
-		$d = new article;
-		$info = $d->findData();
-		
- 		if(!$info || $info['status'] != 1){
-			return  parent::_404();
-		}
-		
-		$m = new common;
-		$info['groupname'] = $m->findAdminGroupData($info['gid']);
-		$info['nickname'] = $m->findAdminUserData($info['uid']);
+        $d = new article;
+        $info = $d->findData();
 
-		$d->hitsData(); //浏览+1
-		
-		view::assign('info',$info);
-		view::assign('cid',$info['cid']);//用于判断栏目路径
-		view::display();
+        if ( !$info || $info['status'] != 1 ) {
+            return  parent::_404();
+        }
+
+        $m = new common;
+        $info['groupname'] = $m->findAdminGroupData( $info['gid'] );
+        $info['nickname'] = $m->findAdminUserData( $info['uid'] );
+
+        $d->hitsData();
+        //浏览+1
+
+        view::assign( 'info', $info );
+        view::assign( 'cid', $info['cid'] );
+        //用于判断栏目路径
+        view::display();
     }
 }
